f0675775028ca9ee8abf18940d99cc6f367f73b6
Fernando Battistella <fernando@fernandobattistella.com.br>
2016-03-13 12:31:40 -0300

    renamed vars and functions for consistency, broke long lines for readability

--- s3_lambda_es.py
+++ s3_lambda_es.py
@@ -21,6 +21,7 @@ import hashlib
 import hmac
 import boto3
 
+########################################################################################################################
 # variables to be changed
 # no https nor trailing slash in this one, just the full hostname of your elasticsearch endpoint
 host = 'search-example-1234567890qwerty.so-meaz-1.es.amazonaws.com'
@@ -28,6 +29,7 @@ region = 'us-east-1'
 # access keys used in the url signing, for some reason the lamba role ones didnt work for me
 access_key = 'access key id'
 secret_key = 'secret key'
+########################################################################################################################
 
 # variables that you should'nt have to change, ever :)
 method = 'POST'
@@ -40,12 +42,12 @@ def sign(key, msg):
     return hmac.new(key, msg.encode("utf-8"), hashlib.sha256).digest()
 
 
-def getSignatureKey(key, date_stamp, regionName, serviceName):
-    kDate = sign(('AWS4' + key).encode('utf-8'), date_stamp)
-    kRegion = sign(kDate, regionName)
-    kService = sign(kRegion, serviceName)
-    kSigning = sign(kService, 'aws4_request')
-    return kSigning
+def get_signature_key(key, date_stamp, region_name, service_name):
+    k_date = sign(('AWS4' + key).encode('utf-8'), date_stamp)
+    k_region = sign(k_date, region_name)
+    k_service = sign(k_region, service_name)
+    k_signing = sign(k_service, 'aws4_request')
+    return k_signing
 
 print 'Lambda function starting'
 
@@ -76,17 +78,28 @@ def lambda_handler(event, context):
     for i in response:
         # leave the boring and useless events out to not flood our elasticsearch db
         # if you want these just remove this line and indent down the block
-        if not(i["eventSource"] == "elasticloadbalancing" and i["eventName"] == "describeInstanceHealth" and i["userIdentity.userName"] == "secret_username"):
-            # prints go to cloudwatch logs ;) easy way to debug or get more information on your logs.
-            # I have my logs on cloudwatch set to erase in 1 day, so I go really verbose here.
+        if not(
+                i["eventSource"] == "elasticloadbalancing" and
+                i["eventName"] == "describeInstanceHealth" and
+                i["userIdentity.userName"] == "secret_username"
+        ):
+            """
+            Prints go to cloudwatch logs ;) easy way to debug or get more information on your logs.
+            I have my logs on cloudwatch set to erase in 1 day, so I go really verbose here.
+            """
+
             print 'Sending event to elasticsearch'
+
             # adds @timestamp field = time of the event
             i["@timestamp"] = i["eventTime"]
+
             # removes .aws.amazon.com from eventsources
             i["eventSource"] = i["eventSource"].split(".")[0]
             data = json.dumps(i)
+
             # defines correct index name based on eventTime, so we have an index for each day on ES
             event_date = i["eventTime"].split("T")[0].replace("-", ".")
+
             # url endpoint for our ES cluster
             url = 'https://'+host+'/logstash-'+event_date+'/cloudtrail/'
             print "url :", url
@@ -98,16 +111,29 @@ def lambda_handler(event, context):
             date_stamp = t.strftime('%Y%m%d')
             canonical_uri = '/logstash-'+event_date+'/cloudtrail/'
             canonical_querystring = ''
-            canonical_headers = 'content-type:' + content_type + '\n' + 'host:' + host + '\n' + 'x-amz-date:' + amz_date + '\n'
+            canonical_headers = 'content-type:' + content_type + '\n' + \
+                                'host:' + host + '\n' + \
+                                'x-amz-date:' + amz_date + '\n'
             signed_headers = 'content-type;host;x-amz-date'
             payload_hash = hashlib.sha256(data).hexdigest()
-            canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash
+            canonical_request = method + '\n' + \
+                                canonical_uri + '\n' + \
+                                canonical_querystring + '\n' + \
+                                canonical_headers + '\n' + \
+                                signed_headers + '\n' + \
+                                payload_hash
             algorithm = 'AWS4-HMAC-SHA256'
             credential_scope = date_stamp + '/' + region + '/' + service + '/' + 'aws4_request'
-            string_to_sign = algorithm + '\n' +  amz_date + '\n' +  credential_scope + '\n' +  hashlib.sha256(canonical_request).hexdigest()
-            signing_key = getSignatureKey(secret_key, date_stamp, region, service)
-            signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
-            authorization_header = algorithm + ' ' + 'Credential=' + access_key + '/' + credential_scope + ', ' +  'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature
+            string_to_sign = algorithm + '\n' + \
+                             amz_date + '\n' + \
+                             credential_scope + '\n' + \
+                             hashlib.sha256(canonical_request).hexdigest()
+            signing_key = get_signature_key(secret_key, date_stamp, region, service)
+            signature = hmac.new(signing_key, string_to_sign.encode('utf-8'), hashlib.sha256).hexdigest()
+            authorization_header = algorithm + ' ' + \
+                                   'Credential=' + access_key + '/' + credential_scope + ', ' + \
+                                   'SignedHeaders=' + signed_headers + ', ' + \
+                                   'Signature=' + signature
             headers = {'Content-Type':content_type,
                        'X-Amz-Date':amz_date,
                        'Authorization':authorization_header}
@@ -135,8 +161,10 @@ def lambda_handler(event, context):
             # if our status code is not successfull, and our retry counter is less than 4
             while req.status_code != 201 and retry_counter < 4:
                 print "retry", retry_counter, "of 3 - failed sending data to elasticsearch:", req.status_code
+
                 # send the data to ES again
                 req = requests.post(url, data=data, headers=headers)
+
                 # if it worked this time, nice! \o/
                 if req.status_code == 201:
                     print "data successfully sent!"
